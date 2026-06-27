a37fceb361f54f5c56758ae85b29ad3dc0881a78
Tijn Schutte <212696143+tijn-hedgehog@users.noreply.github.com>
2026-06-27 13:40:50 +0200

    Use vertical layout for telescope-undo on smaller screens

--- lua/custom/plugins/telescope-undo.lua
+++ lua/custom/plugins/telescope-undo.lua
@@ -5,6 +5,16 @@ return {
     { '<leader>su', '<cmd>Telescope undo<cr>', desc = '[S]earch [U]ndo history' },
   },
   config = function()
+    require('telescope').setup {
+      extensions = {
+        undo = {
+          layout_strategy = 'vertical',
+          layout_config = {
+            preview_height = 0.6,
+          },
+        },
+      },
+    }
     require('telescope').load_extension 'undo'
   end,
 }
