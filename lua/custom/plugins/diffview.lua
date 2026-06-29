bb43fa93d5acd124cc5f14815ccf1744b1e66589
Tijn Schutte <212696143+tijn-hedgehog@users.noreply.github.com>
2026-06-29 09:39:31 +0200

    Make diffview keymaps toggle, disable icons, and add flash treesitter search

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -3,10 +3,43 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   cmd = { 'DiffviewOpen', 'DiffviewFileHistory' },
   keys = {
-    { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = '[G]it [D]iff view' },
-    { '<leader>gh', '<cmd>DiffviewFileHistory %<cr>', desc = '[G]it file [H]istory' },
-    { '<leader>gH', '<cmd>DiffviewFileHistory<cr>', desc = '[G]it repo [H]istory' },
-    { '<leader>gq', '<cmd>DiffviewClose<cr>', desc = '[G]it diff [Q]uit' },
+    {
+      '<leader>gd',
+      function()
+        if next(require('diffview.lib').views) then
+          vim.cmd 'DiffviewClose'
+        else
+          vim.cmd 'DiffviewOpen'
+        end
+      end,
+      desc = '[G]it [D]iff toggle',
+    },
+    {
+      '<leader>gh',
+      function()
+        if next(require('diffview.lib').views) then
+          vim.cmd 'DiffviewClose'
+        else
+          vim.cmd 'DiffviewFileHistory %'
+        end
+      end,
+      desc = '[G]it file [H]istory toggle',
+    },
+    {
+      '<leader>gH',
+      function()
+        if next(require('diffview.lib').views) then
+          vim.cmd 'DiffviewClose'
+        else
+          vim.cmd 'DiffviewFileHistory'
+        end
+      end,
+      desc = '[G]it repo [H]istory toggle',
+    },
   },
-  opts = {},
+  opts = {
+    icons = { folder_closed = '>', folder_open = 'v' },
+    signs = { fold_closed = '>', fold_open = 'v' },
+    use_icons = false,
+  },
 }
--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -5,6 +5,7 @@ return {
     { 's', function() require('flash').jump() end, mode = { 'n', 'x', 'o' }, desc = 'Flash' },
     { 'S', function() require('flash').treesitter() end, mode = { 'n', 'x', 'o' }, desc = 'Flash Treesitter' },
     { 'r', function() require('flash').remote() end, mode = 'o', desc = 'Remote Flash' },
+    { 'R', function() require('flash').treesitter_search() end, mode = 'o', desc = 'Treesitter Search' },
     { '<c-s>', function() require('flash').toggle() end, mode = 'c', desc = 'Toggle Flash Search' },
   },
   opts = {},
