d43e7f37de172da20abb3cd5e6d60ac036581a88
Tijn Schutte <212696143+tijn-hedgehog@users.noreply.github.com>
2026-07-02 12:38:17 +0200

    Use flex layout for live_grep, drop duplicate sg keymap
    <leader>fg now switches to a vertical layout (preview above results)
    on narrow windows via Telescope's flex strategy. <leader>sg was an
    exact duplicate of live_grep with no options, so it's removed in
    favor of the single <leader>fg mapping.

--- init.lua
+++ init.lua
@@ -487,8 +487,15 @@ require('lazy').setup({
       )
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
+      vim.keymap.set('n', '<leader>fg', function()
+        builtin.live_grep {
+          layout_strategy = 'flex',
+          layout_config = {
+            flip_columns = 120,
+            vertical = { preview_height = 0.6 },
+          },
+        }
+      end, { desc = '[F]ind by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
