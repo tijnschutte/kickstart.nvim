e9a3a1e654f69c9cef544e671b2cc10c5b5c6010
Tijn Schutte <212696143+tijn-hedgehog@users.noreply.github.com>
2026-07-23 11:17:29 +0200

    Fix diffview toggle going dead outside the diffview tab
    The toggle checked `lib.views`, which is global, but `:DiffviewClose`
    only closes a view in the current tabpage. Once you left the diffview
    tab the keymap took the close branch and did nothing at all.
    
    Check `lib.get_current_view()` instead, jump to an existing view living
    in another tabpage, and share one toggle helper across all three maps.

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -1,3 +1,31 @@
+local opts = {
+  icons = { folder_closed = '>', folder_open = 'v' },
+  signs = { fold_closed = '>', fold_open = 'v' },
+  use_icons = false,
+}
+
+-- `:DiffviewClose` only closes a view living in the *current* tabpage, so the
+-- toggle has to look at the current tabpage too. Checking `lib.views` (which is
+-- global) would make the key a no-op whenever you wander off the diffview tab.
+local function toggle(open)
+  return function()
+    local lib = require 'diffview.lib'
+
+    if lib.get_current_view() then
+      vim.cmd 'DiffviewClose'
+      return
+    end
+
+    local view = lib.views[1]
+    if view and vim.api.nvim_tabpage_is_valid(view.tabpage) then
+      vim.api.nvim_set_current_tabpage(view.tabpage)
+      return
+    end
+
+    open()
+  end
+end
+
 return {
   'sindrets/diffview.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -5,41 +33,25 @@ return {
   keys = {
     {
       '<leader>gd',
-      function()
-        if next(require('diffview.lib').views) then
-          vim.cmd 'DiffviewClose'
-        else
-          vim.cmd 'DiffviewOpen'
-        end
-      end,
+      toggle(function()
+        local layout = vim.o.columns < 120 and 'diff2_vertical' or 'diff2_horizontal'
+        require('diffview').setup(vim.tbl_deep_extend('force', opts, {
+          view = { default = { layout = layout } },
+        }))
+        vim.cmd 'DiffviewOpen'
+      end),
       desc = '[G]it [D]iff toggle',
     },
     {
       '<leader>gh',
-      function()
-        if next(require('diffview.lib').views) then
-          vim.cmd 'DiffviewClose'
-        else
-          vim.cmd 'DiffviewFileHistory %'
-        end
-      end,
+      toggle(function() vim.cmd 'DiffviewFileHistory %' end),
       desc = '[G]it file [H]istory toggle',
     },
     {
       '<leader>gH',
-      function()
-        if next(require('diffview.lib').views) then
-          vim.cmd 'DiffviewClose'
-        else
-          vim.cmd 'DiffviewFileHistory'
-        end
-      end,
+      toggle(function() vim.cmd 'DiffviewFileHistory' end),
       desc = '[G]it repo [H]istory toggle',
     },
   },
-  opts = {
-    icons = { folder_closed = '>', folder_open = 'v' },
-    signs = { fold_closed = '>', fold_open = 'v' },
-    use_icons = false,
-  },
+  opts = opts,
 }
